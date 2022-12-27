5da7990cbfff6233f6fb43cc3ebbccc000596139
Zangeronimo <zangeronimo@gmail.com>
2022-12-27 08:25:27 -0300

    chore: configure styled-components and phosphor icons

---
+++ src/styles/theme.ts
@@ -0,0 +1,29 @@
+export default {
+  colors: {
+    'red-dark': '#BF3B44',
+    'red-mid': '#F3BABD',
+    'red-light': '#F4E6E7',
+
+    'green-dark': '#639339',
+    'green-mid': '#CBE4B4',
+    'green-light': '#E5F0DB',
+    'grey-900': '#1B1D1E',
+    'grey-800': '#333638',
+    'grey-700': '#5C6265',
+    'grey-500': '#B9BBBC',
+    'grey-300': '#DDDEDF',
+    'grey-200': '#eff0f0',
+    'grey-100': '#FAFAFA',
+
+    white: '#FFFFFF',
+    background: '#DEDEDE',
+  },
+  fonts: {
+    xs: '12px',
+    sm: '14px',
+    md: '16px',
+    lg: '18px',
+    xl: '24px',
+    hg: '32px',
+  },
+};
